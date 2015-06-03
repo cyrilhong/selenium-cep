8e2114b1c0d75b0425f3f87f0bf4495edbb378c8
Jace Ju <jaceju@gmail.com>
2015-06-03 16:19:00 +0800

    Remove unused code

--- tests/PHPWebDriverTest.php
+++ tests/PHPWebDriverTest.php
@@ -32,26 +32,4 @@ class PHPWebDriverTest extends PHPUnit_Framework_TestCase
         // checking that page title contains word 'GitHub'
         $this->assertContains('GitHub · Build software better, together.', $this->webDriver->getTitle());
     }
-
-    // public function testSearch()
-    // {
-    //     $this->webDriver->get($this->url);
-    //     // find search field by its id
-    //     $search = $this->webDriver->findElement(WebDriverBy::id('js-command-bar-field'));
-    //     $search->click();
-    //     // typing into field
-    //     $this->webDriver->getKeyboard()->sendKeys('php-webdriver');
-    //     // pressing "Enter"
-    //     $this->webDriver->getKeyboard()->pressKey(WebDriverKeys::ENTER);
-    //     $firstResult = $this->webDriver->findElement(
-    //     // some CSS selectors can be very long:
-    //         WebDriverBy::cssSelector('li.public:nth-child(1) > h3:nth-child(3) > a:nth-child(1) > em:nth-child(2)')
-    //     );
-    //     $firstResult->click();
-    //     // we expect that facebook/php-webdriver was the first result
-    //     $this->assertContains("php-webdriver", $this->webDriver->getTitle());
-    //     $this->assertEquals('https://github.com/facebook/php-webdriver', $this->webDriver->getCurrentURL());
-    //     $this->assertElementNotFound(WebDriverBy::className('avatar'));
-    //     // $this->waitForUserInput();
-    // }
 }
